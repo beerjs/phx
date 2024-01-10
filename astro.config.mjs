d625a848e6c15cd4df5a46fb419fd27b638df00e
Darko Bozhinovski <hello@darko.io>
2024-01-10 12:37:07 +0100

    feat: fixes accessibility errors; adds sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import solidJs from '@astrojs/solid-js';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
+  site: 'https://beerjs.tech',
+  integrations: [tailwind(), solidJs(), sitemap()],
 });
